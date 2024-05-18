8f7ade1e42c2905d7cc80fb2d009ba37ba546b04
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-05-18 23:58:44 +0300

    add cell to lastUpload

--- TabBar/AboutPersonViewControllers/LastUploads/LastUploadsViewController.swift
+++ TabBar/AboutPersonViewControllers/LastUploads/LastUploadsViewController.swift
@@ -9,14 +9,16 @@ import UIKit
 
 class LastUploadsViewController: UIViewController {
     
+    let tableView = UITableView()
+    
     let viewModel: LastUploadsModel
+    
+    
     
     init(viewModel: LastUploadsModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-    
-    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -29,11 +31,18 @@ class LastUploadsViewController: UIViewController {
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Назад".localizedText(), style: .done, target: self, action: #selector(close))
         
+        tableView.register(LastUploadTableViewCell.self, forCellReuseIdentifier: LastUploadTableViewCell.identyfire)
         
         navigationItem.title = "Последние Загрузки"
         view.backgroundColor = .white
      
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+   
     
     
     
@@ -42,3 +51,21 @@ class LastUploadsViewController: UIViewController {
     }
     
 }
+
+extension LastUploadsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: LastUploadTableViewCell.identyfire, for: indexPath) as! LastUploadTableViewCell
+        
+       
+        
+        return cell
+    }
+    
+    
+}
+

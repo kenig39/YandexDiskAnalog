6d154cab4e130b62759727641f6eeb6c78b7c782
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-06-18 23:04:01 +0300

    some code

--- CoreData/CoreDataManager.swift
+++ CoreData/CoreDataManager.swift
@@ -17,4 +17,35 @@ class CoreDataManager {
     lazy var context: NSManagedObjectContext = {
         return persistentContainer.viewContext
     }()
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "SkillBoxDisk")
+    }
+    
+    func loadCoreDataRecently() {
+        persistentContainer.loadPersistentStores { persistent, error in
+            if let error = error {
+                print(error)
+            } else {
+                print("Loaded CoreData")
+            }
+        }
+    }
+    
+    
+    func deleteAllData(_ entity: String) {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+        fetchRequest.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(fetchRequest)
+            for object in result {
+                guard let objectData = object as? NSManagedObject else {continue}
+                context.delete(objectData)
+                try? context.save()
+            }
+            print("All CoreData \(entity) deleted")
+        } catch let error {
+            print("delete all data in \(entity), error \(error)")
+        }
+    }
 }

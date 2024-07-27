dea4fbc17cdee1206bc654214be73d6a630ad740
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-07-27 20:58:01 +0300

    add core data files

---
+++ CoreData/CoreDataViewControllers/LastFilesCoreData/LastFilesModelSave.swift
@@ -0,0 +1,17 @@
+//
+//  LastFilesCoreData.swift
+//  SkillBoxDisk
+//
+//  Created by Alexander on 27.07.2024.
+//
+
+import Foundation
+import CoreData
+
+
+final class LastFilesModelSave {
+    
+    
+    
+}
+
---
+++ CoreData/CoreDataViewControllers/PublishFilesCoreData/PublishFilesModelSave.swift
@@ -0,0 +1,14 @@
+//
+//  PublishFilesModelSave.swift
+//  SkillBoxDisk
+//
+//  Created by Alexander on 27.07.2024.
+//
+
+import Foundation
+import CoreData
+
+final class PublishModelSave {
+    
+    
+}
